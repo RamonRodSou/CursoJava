18f843d8f778c5b52db37d8299b716152775f17d
Ramon Rodrigues <ramonrodsouza@gmail.com>
2024-10-23 15:15:35 -0300

    feat: Comecando o curso Spring Data JPA, e criando enum e classe Serie

---
+++ faculdade/src/Principal/Main.java
@@ -0,0 +1,35 @@
+package Principal;
+
+public class Main {
+
+    public static int fase = 0;
+
+    public static void main(String[] args) {
+
+        // Questões
+
+        System.out.println("Questao 01");
+        int x = 1; // 2, 3, 4
+        do {
+            if (x % 5 == 0)
+                continue;
+            if (x % 4 == 0) // parou pq 4 % 4 == 0 zero
+                break;
+
+            System.out.println(x++ + " "); // imprimi 1 e soma + 2 , imprime 2 e soma +1 = 3, imprime 3 e soma +1 = 4
+        } while (x <= 7); // ok
+
+        // vai imprimir 1, 2, 3
+
+        System.out.println("--------------------------------");
+        System.out.println("Questao 02");
+
+        fase = 5;
+        for (int i = fase; i > 1; i--) {
+            i++;
+            System.out.println(fase);
+        }
+
+    }
+
+}
